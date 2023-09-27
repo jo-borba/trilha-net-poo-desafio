56127cb1094f6c726821af09b4ab78fafa487635
joao.borba <joao.borba@dclogisticsbrasil.com>
2023-09-26 23:13:09 -0300

    Commit Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("333322222332", "Nokia 3570", "15417087a8781a57mxx33", 2048);
+Iphone iphone = new Iphone("1111118455", "Iphone 15 PRO MAX", "7777487434d848f", 5120);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Linkedin");
+
+Console.WriteLine();
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
